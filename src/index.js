5beee5b98f8d540782ae20443bf0c90c93db2ddc
AIGERIM <toleush.aigerim@gmail.com>
2020-10-03 21:27:04 +0600

    feat: make typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
+  
+  if (Array.isArray(array) && array.length){
+    let minN = array[0];
+    for (var i=0; i < array.length; i++ ){
+      if(minN > array[i]){
+        minN = array[i];
+      }
+    }
+    return minN;
+  }
   return 0;
+  
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length){
+    let minN = array[0];
+    for (var i=0; i < array.length; i++ ){
+      if(minN < array[i]){
+        minN = array[i];
+      }
+    }
+    return minN;
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length){
+    let minN = array[0];
+    let avg = 0;
+    let sum = 0;
+    for (var i=0; i < array.length; i++ ){
+      sum += array[i];
+    }
+    avg = sum/array.length;
+    return avg;
+  }
   return 0;
 }
